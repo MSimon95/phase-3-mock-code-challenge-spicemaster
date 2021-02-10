f488c8e259a738d687577781c8c44af924b5e7e5
github username <github email address>
2021-02-10 11:31:50 -0500

    ingredient

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const spiceblendsUrl = 'http://localhost:3000/spiceblends'
 const ingredientsUrl = 'http://localhost:3000/ingredients'
+
 const spiceDetails = document.getElementById("spice-blend-detail")
 const spiceImages = document.getElementById("spice-images")
 
@@ -8,35 +9,34 @@ function fetchSpices() {
         .then (res => res.json())
         .then (spiceInfo => {
             spiceInfo.forEach(spice => renderOneSpice(spice));
+
             });
         }
 
 fetchSpices()
 
  function renderOneSpice (spice) {
-    const thisSpiceImage = spiceDetails.querySelector("img.detail-image")
-    const thisSpiceTitle = spiceDetails.querySelector("h2.title")
-    thisSpiceImage.src = spice.image;
-    thisSpiceImage.alt = spice.id
-    thisSpiceTitle.textContent = spice.title
-    const thisSpiceIngredients = spiceDetails.querySelector("div.ingredients-container")
-    console.log(spice)
- }
-
-//   function renderOneToy (toy){
-//     const card = document.createElement('div')
-//     card.className = 'toy-card'
-//   card.dataset.id = toy.id
-//   let image = document.createElement(`img`)
-//   image.setAttribute('src', toy.image)
-//   card.innerHTML =`
-//   <div class="card">
-//     <h2${toy.name}</h2>
-//     <img src=${toy.image} class='toy-avatar' </>
-//     <p> ${toy.likes}</p>
-//     <button class="like-btn">Like<3</button>
-//   </div> `
-
-// collection.append(card)
-// }
-// renderAllToys()
+    fetch (`http://localhost:3000/spiceblends/${spice.id}`)
+    .then (res => res.json())
+    .then (spiceWithIngredient => {
+            console.log(spiceWithIngredient)
+            spiceDetails.dataset.id = spiceWithIngredient.id;
+            const thisSpiceImage = spiceDetails.querySelector("img.detail-image")
+            const thisSpiceTitle = spiceDetails.querySelector("h2.title")
+            const thisSpiceIngredients = spiceDetails.querySelector("div.ingredients-container")
+            thisSpiceImage.src = spiceWithIngredient.image;
+            thisSpiceImage.alt = spiceWithIngredient.id
+            thisSpiceTitle.textContent = spiceWithIngredient.title
+            spiceWithIngredient.ingredients.forEach(ingredient => {
+                console.log(ingredient)
+                if (spice.id === ingredient.spiceblendId ){  
+                const thisIngredients = document.createElement("li")
+                thisIngredients.textContent = ingredient.name
+                thisSpiceIngredients.append(thisIngredients);}
+         
+            });
+        })
+    }
+    
+    
+ 
